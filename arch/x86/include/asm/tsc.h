393a64b286cc371f63959b71fe888ff5f8935f19
Steven Noonan <steven@uplinklabs.net>
2021-11-17 00:25:26 -0800

    x86: implement tsc=directsync for systems without IA32_TSC_ADJUST
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>
    [fwd-port to v6.5]
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
    Signed-off-by: Robert Beckett <bob.beckett@collabora.com>

--- arch/x86/include/asm/tsc.h
+++ arch/x86/include/asm/tsc.h
@@ -93,6 +93,7 @@ extern unsigned long native_calibrate_tsc(void);
 extern unsigned long long native_sched_clock_from_tsc(u64 tsc);
 
 extern int tsc_clocksource_reliable;
+extern int tsc_allow_direct_sync;
 extern bool tsc_async_resets;
 
 /*
