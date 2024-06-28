4f1fe1ad5eb30579ca3daff13f233dbab00f24f2
Orlando Chamberlain <orlandoch.dev@gmail.com>
2024-06-28 04:43:50 +0000

    i915: 4 lane quirk for mbp15,1
    Needed to use iGPU when dGPU was boot GPU
    
    Patch written by Kerem Karabay <kekrby@gmail.com>

--- drivers/gpu/drm/i915/display/intel_quirks.h
+++ drivers/gpu/drm/i915/display/intel_quirks.h
@@ -23,6 +23,7 @@ enum intel_quirk_id {
 	QUIRK_EDP_LIMIT_RATE_HBR2,
 	QUIRK_DISABLE_EDP_PANEL_REPLAY,
 	QUIRK_DISABLE_PSR2,
+	QUIRK_DDI_A_FORCE_4_LANES,
 };
 
 void intel_init_quirks(struct intel_display *display);
