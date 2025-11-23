0d09957e6d7013e1f8dd7640f62e0a1971f2c054
Xie Yuanbin <qq570070308@gmail.com>
2025-11-23 20:18:27 +0800

    sched/core: Make finish_task_switch() and its subfunctions always inline
    finish_task_switch() is a hot code path in context switching.
    When spectre_v2_user is enabled, kernel is likely to perform branch
    prediction hardening inside switch_mm_irqs_off(). finish_task_switch()
    is right after switch_mm_irqs_off(), so the performance here is
    greatly affected by function calls and branch jumps.
    
    Make finish_task_switch() always inline to optimize performance.
    
    After finish_task_switch() is changed as always inline, the number of
    calling points of subfunctions increases. According to the compiler
    optimization strategy, subfunctions that were originally inline may no
    longer be inline.
    
    Also make the subfunctions of finish_task_stwitch() always inline to
    prevent performance degradation.
    
    There is a improvement in the performace of finish_task_switch(). When
    spectre v2 is enabled, the improvement is significant.
    
    The following are testing results from intel i5-8300h@4Ghz (x86):
    Time spent on calling finish_task_switch(), the unit is tsc from x86:
     | test scenario             | old   | new   | delta          |
     | gcc 15.2                  | 13.94 | 12.40 | 1.54  (-11.1%) |
     | gcc 15.2 + spectre_v2     | 24.78 | 13.70 | 11.08 (-44.7%) |
     | clang 21.1.4              | 13.90 | 12.71 | 1.19  (- 8.6%) |
     | clang 21.1.4 + spectre_v2 | 29.01 | 18.91 | 10.1  (-34.8%) |
    
    There is a minor improvement in the size of .text section in vmlinux,
    the unit is bytes:
     | test scenario             | old      | new      | delta |
     | gcc 15.2                  | 16208096 | 16208736 | 640   |
     | clang 21.1.4              | 17943328 | 17944224 | 896   |
    
    No size changes were found on bzImage.
    
    Signed-off-by: Xie Yuanbin <qq570070308@gmail.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Segher Boessenkool <segher@kernel.crashing.org>
    Cc: David Hildenbrand (Red Hat) <david@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: H. Peter Anvin (Intel) <hpa@zytor.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4952,7 +4952,7 @@ static inline void prepare_task(struct task_struct *next)
 	WRITE_ONCE(next->on_cpu, 1);
 }
 
-static inline void finish_task(struct task_struct *prev)
+static __always_inline void finish_task(struct task_struct *prev)
 {
 	/*
 	 * This must be the very last reference to @prev from this CPU. After
@@ -4968,7 +4968,7 @@ static inline void finish_task(struct task_struct *prev)
 	smp_store_release(&prev->on_cpu, 0);
 }
 
-static void do_balance_callbacks(struct rq *rq, struct balance_callback *head)
+static __always_inline void do_balance_callbacks(struct rq *rq, struct balance_callback *head)
 {
 	void (*func)(struct rq *rq);
 	struct balance_callback *next;
@@ -5003,7 +5003,7 @@ struct balance_callback balance_push_callback = {
 	.func = balance_push,
 };
 
-static inline struct balance_callback *
+static __always_inline struct balance_callback *
 __splice_balance_callbacks(struct rq *rq, bool split)
 {
 	struct balance_callback *head = rq->balance_callback;
@@ -5033,7 +5033,7 @@ struct balance_callback *splice_balance_callbacks(struct rq *rq)
 	return __splice_balance_callbacks(rq, true);
 }
 
-void __balance_callbacks(struct rq *rq, struct rq_flags *rf)
+__always_inline void __balance_callbacks(struct rq *rq, struct rq_flags *rf)
 {
 	if (rf)
 		rq_unpin_lock(rq, rf);
@@ -5077,7 +5077,7 @@ prepare_lock_switch(struct rq *rq, struct task_struct *next, struct rq_flags *rf
 	__acquire(__rq_lockp(this_rq()));
 }
 
-static inline void finish_lock_switch(struct rq *rq)
+static __always_inline void finish_lock_switch(struct rq *rq)
 	__releases(__rq_lockp(rq))
 {
 	/*
@@ -5111,7 +5111,7 @@ static inline void kmap_local_sched_out(void)
 #endif
 }
 
-static inline void kmap_local_sched_in(void)
+static __always_inline void kmap_local_sched_in(void)
 {
 #ifdef CONFIG_KMAP_LOCAL
 	if (unlikely(current->kmap_ctrl.idx))
@@ -5165,7 +5165,7 @@ prepare_task_switch(struct rq *rq, struct task_struct *prev,
  * past. 'prev == current' is still correct but we need to recalculate this_rq
  * because prev may have moved to another CPU.
  */
-static struct rq *finish_task_switch(struct task_struct *prev)
+static __always_inline struct rq *finish_task_switch(struct task_struct *prev)
 	__releases(__rq_lockp(this_rq()))
 {
 	struct rq *rq = this_rq();
