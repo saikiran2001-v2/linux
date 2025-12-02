d49b70b9073a784e455540bb09b22b90084c65b0
Yaroslav Bolyukin <iam@lach.pw>
2025-12-02 12:02:14 +0100

    drm/edid: MSO should only be used for non-eDP displays
    As per DisplayID v2.1a spec:
    If Offset 06h[2:0] is programmed to 001b (External DisplayPort), this
    field shall be cleared to 00b (Not supported).
    
    Link: https://lore.kernel.org/lkml/3abc1087618c822e5676e67a3ec2e64e506dc5ec@intel.com/
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/drm_displayid_internal.h
+++ drivers/gpu/drm/drm_displayid_internal.h
@@ -142,8 +142,12 @@ struct displayid_formula_timing_block {
 	struct displayid_formula_timings_9 timings[];
 } __packed;
 
+#define DISPLAYID_VESA_DP_TYPE		GENMASK(2, 0)
 #define DISPLAYID_VESA_MSO_OVERLAP	GENMASK(3, 0)
 #define DISPLAYID_VESA_MSO_MODE		GENMASK(6, 5)
+
+#define DISPLAYID_VESA_DP_TYPE_EDP	0
+#define DISPLAYID_VESA_DP_TYPE_DP	1
 
 struct displayid_vesa_vendor_specific_block {
 	struct displayid_block base;
