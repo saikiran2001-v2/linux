744326cf5f62eee273fac0ab0fa053522ed20ae5
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2026-01-14 20:28:05 +0100

    drm/amd/display: Rename PCON adaptive sync types
    [Why]
    PCONs support sending out HDMI VRR infopackets on their own and this
    makes this types not specific to FreeSync
    
    [How]
    Make the name more generic for the upcoming HDMI VRR over PCON
    implementation
    
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
    Tested-by: Bernhard Berger <bernhard.berger@gmail.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/display/modules/inc/mod_info_packet.h
+++ drivers/gpu/drm/amd/display/modules/inc/mod_info_packet.h
@@ -48,6 +48,14 @@ void mod_build_vsc_infopacket(const struct dc_stream_state *stream,
 void mod_build_hf_vsif_infopacket(const struct dc_stream_state *stream,
 		struct dc_info_packet *info_packet, int ALLMEnabled, int ALLMValue);
 
+enum adaptive_sync_type {
+	ADAPTIVE_SYNC_TYPE_NONE             = 0,
+	ADAPTIVE_SYNC_TYPE_DP               = 1,
+	ADAPTIVE_SYNC_TYPE_PCON_ALLOWED     = 2,
+	ADAPTIVE_SYNC_TYPE_PCON_NOT_ALLOWED = 3,
+	ADAPTIVE_SYNC_TYPE_EDP              = 4,
+};
+
 enum adaptive_sync_sdp_version {
 	AS_SDP_VER_0 = 0x0,
 	AS_SDP_VER_1 = 0x1,
