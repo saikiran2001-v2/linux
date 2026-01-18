7961803c961b087a240e1cb267e99a1f3f0f757a
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2026-01-18 14:10:22 +0100

    drm/amd/display: Refactor PCON VRR compatibility check
    [Why]
    DP->HDMI PCONs prevously entered the DP path
    
    [How]
    Restructure amdgpu_dm_update_freesync_caps() and move
    dm_get_adaptive_sync_support_type() to dm_helpers_is_vrr_pcon_allowed()
    to better reflect what this function does. It never actually gave us any
    other info.
    
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_helpers.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_helpers.c
@@ -1564,43 +1564,55 @@ void dm_helpers_dp_mst_update_branch_bandwidth(
 	// TODO
 }
 
-static bool dm_is_freesync_pcon_whitelist(const uint32_t branch_dev_id)
+STATIC_IFN_KUNIT const uint32_t dm_freesync_pcon_whitelist[] = {
+	DP_BRANCH_DEVICE_ID_0060AD,
+	DP_BRANCH_DEVICE_ID_00E04C,
+	DP_BRANCH_DEVICE_ID_90CC24,
+	DP_BRANCH_DEVICE_ID_001CF8,
+	DP_BRANCH_DEVICE_ID_001FF2,
+};
+EXPORT_IF_KUNIT(dm_freesync_pcon_whitelist);
+
+STATIC_IFN_KUNIT uint32_t dm_freesync_pcon_whitelist_count(void)
 {
-	bool ret_val = false;
+	return ARRAY_SIZE(dm_freesync_pcon_whitelist);
+}
+EXPORT_IF_KUNIT(dm_freesync_pcon_whitelist_count);
 
-	switch (branch_dev_id) {
-	case DP_BRANCH_DEVICE_ID_0060AD:
-	case DP_BRANCH_DEVICE_ID_00E04C:
-	case DP_BRANCH_DEVICE_ID_90CC24:
-	case DP_BRANCH_DEVICE_ID_001CF8:
-	case DP_BRANCH_DEVICE_ID_001FF2:
-		ret_val = true;
-		break;
-	default:
-		break;
-	}
+static bool dm_helpers_is_vrr_pcon_allowlist(const uint32_t branch_dev_id)
+{
+	u32 i;
 
-	return ret_val;
+	for (i = 0; i < dm_freesync_pcon_whitelist_count(); i++)
+		if (dm_freesync_pcon_whitelist[i] == branch_dev_id)
+			return true;
+
+	return false;
 }
 
-enum adaptive_sync_type dm_get_adaptive_sync_support_type(struct dc_link *link)
+STATIC_IFN_KUNIT bool dm_is_freesync_pcon_whitelist(const uint32_t branch_dev_id)
 {
-	struct dpcd_caps *dpcd_caps = &link->dpcd_caps;
-	enum adaptive_sync_type as_type = ADAPTIVE_SYNC_TYPE_NONE;
-
-	switch (dpcd_caps->dongle_type) {
-	case DISPLAY_DONGLE_DP_HDMI_CONVERTER:
-		if (dpcd_caps->adaptive_sync_caps.dp_adap_sync_caps.bits.ADAPTIVE_SYNC_SDP_SUPPORT == true &&
-			dpcd_caps->allow_invalid_MSA_timing_param == true &&
-			dm_is_freesync_pcon_whitelist(dpcd_caps->branch_dev_id))
-			as_type = FREESYNC_TYPE_PCON_IN_WHITELIST;
-		break;
-	default:
-		break;
-	}
-
-	return as_type;
+	return dm_helpers_is_vrr_pcon_allowlist(branch_dev_id);
 }
+EXPORT_IF_KUNIT(dm_is_freesync_pcon_whitelist);
+
+bool dm_helpers_is_vrr_pcon_compatible(const struct dc_link *link)
+{
+	if (link->dpcd_caps.dongle_type != DISPLAY_DONGLE_DP_HDMI_CONVERTER)
+		return false;
+
+	if (!link->dpcd_caps.allow_invalid_MSA_timing_param)
+		return false;
+
+	if (!link->dpcd_caps.adaptive_sync_caps.dp_adap_sync_caps.bits.ADAPTIVE_SYNC_SDP_SUPPORT)
+		return false;
+
+	if (dm_helpers_is_vrr_pcon_allowlist(link->dpcd_caps.branch_dev_id))
+		return true;
+
+	return false;
+}
+EXPORT_IF_KUNIT(dm_helpers_is_vrr_pcon_compatible);
 
 bool dm_helpers_is_fullscreen(struct dc_context *ctx, struct dc_stream_state *stream)
 {
@@ -1703,7 +1715,7 @@ void dm_helpers_read_mccs_caps(struct dc_context *ctx, struct dc_link *link,
 		if (dc_is_dp_signal(link->connector_signal)) {
 			if ((dpcd_caps->dpcd_rev.raw >= DPCD_REV_14) &&
 				(dpcd_caps->dongle_type == DISPLAY_DONGLE_DP_HDMI_CONVERTER) &&
-				dm_is_freesync_pcon_whitelist(dpcd_caps->branch_dev_id) &&
+				dm_helpers_is_vrr_pcon_allowlist(dpcd_caps->branch_dev_id) &&
 				(dpcd_caps->adaptive_sync_caps.dp_adap_sync_caps.bits.ADAPTIVE_SYNC_SDP_SUPPORT == true))
 				mccs_op = true;
 
