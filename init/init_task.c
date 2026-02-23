881454befbf032fc94e72c9063485fa6bde0eeea
Eric Naim <dnaim@cachyos.org>
2026-02-23 21:53:12 +0800

    CACHY: Decrease timer_slack_ns default
    Link: https://github.com/marioroy/cachymod/blob/f0c595b9a2ae8bbcecdb3ab031f633595ed57976/linux-cachymod-6.18/0260-fair-update-cachy-mods.patch
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- init/init_task.c
+++ init/init_task.c
@@ -170,7 +170,11 @@ struct task_struct init_task __aligned(L1_CACHE_BYTES) = {
 	INIT_CPU_TIMERS(init_task)
 	.pi_lock	= __RAW_SPIN_LOCK_UNLOCKED(init_task.pi_lock),
 	.blocked_lock	= __RAW_SPIN_LOCK_UNLOCKED(init_task.blocked_lock),
+#ifdef CONFIG_CACHY
+	.timer_slack_ns = 35000, /* 35 usec default slack */
+#else
 	.timer_slack_ns = 50000, /* 50 usec default slack */
+#endif /* CONFIG_CACHY */
 	.thread_pid	= &init_struct_pid,
 	.thread_node	= LIST_HEAD_INIT(init_signals.thread_head),
 #ifdef CONFIG_AUDIT
