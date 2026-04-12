e2e4ba30d34f1b70d48993439c61cd8bc6de7102
Eric Naim <dnaim@cachyos.org>
2026-04-12 16:18:45 +0800

    Kbuild: Enable LLVM machine pipeliner
    This is the LLVM equivalent to GCC's swing modulo scheduling. Enable it
    as well to get the similar optimizations when building with clang.
    
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

--- drivers/acpi/processor_idle.c
+++ drivers/acpi/processor_idle.c
@@ -1418,6 +1418,7 @@ void acpi_processor_power_init(struct acpi_processor *pr)
 	if (disabled_by_idle_boot_param())
 		return;
 
+#ifdef CONFIG_X86
 	/*
 	 * The bus master check on AMD is observed to be very costly and results
 	 * in not only increased power consumption, but also lots of CPU cycles
@@ -1428,6 +1429,7 @@ void acpi_processor_power_init(struct acpi_processor *pr)
 		bm_check_disable = true;
 		pr_info("Enabled processor.bm_check_disable for AMD cpuidle\n");
 	}
+#endif
 
 	if (!acpi_processor_get_power_info(pr))
 		pr->flags.power_setup_done = 1;
