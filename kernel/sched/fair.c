eb66b418831b1607c7f5f27de67edcaaeac9e8f3
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2026-05-14 11:34:39 -0700

    sched/fair: Allow load balancing between CPUs of identical capacity
    sched_balance_find_src_rq() avoids selecting a runqueue with a single
    running task as busiest if doing so results in migrating the task to a
    CPU with less than ~5% of extra capacity. It also unintentionally
    prevents migrations between CPUs of identical capacity.
    
    When CONFIG_SCHED_CLUSTER is enabled, load should be balanced across
    clusters of CPUs with the same capacity. Allowing migration between CPUs
    of identical capacity is necessary to meet this goal.
    
    Use arch_scale_cpu_capacity() to reflect architectural capacity, excluding
    runtime reductions due to side activity or thermal pressure. Guard this
    check with the sched_cluster_active static key so that systems without
    cluster topology are unaffected.
    
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11960,8 +11960,14 @@ static struct rq *sched_balance_find_src_rq(struct lb_env *env,
 		 * eventually lead to active_balancing high->low capacity.
 		 * Higher per-CPU capacity is considered better than balancing
 		 * average load.
+		 *
+		 * CONFIG_SCHED_CLUSTER requires balancing load across clusters
+		 * of identical capacity. Use architectural capacity to ignore
+		 * runtime variability.
 		 */
 		if (env->sd->flags & SD_ASYM_CPUCAPACITY &&
+		    (!static_branch_unlikely(&sched_cluster_active) ||
+		     arch_scale_cpu_capacity(env->dst_cpu) != arch_scale_cpu_capacity(i)) &&
 		    !capacity_greater(capacity_of(env->dst_cpu), capacity) &&
 		    nr_running == 1)
 			continue;
