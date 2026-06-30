5d0aab124b5920ab6b57b0a80af8e49c2a476bdd
Eric Naim <dnaim@cachyos.org>
2026-06-30 14:12:19 +0700

    ALSA: Add AW88399 side codec support
    Link: https://github.com/nadimkobeissi/16iax10h-linux-sound-saga
    Signed-off-by: Eric Naim <dnaim@cachyos.org>

---
+++ sound/hda/codecs/side-codecs/aw88399_hda_property.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only
+ *
+ * aw88399_hda_property.h -- AW88399 HDA property driver
+ *
+ * Based on cs35l41_hda_property.h
+ *
+ * Author: Marco Giunta
+ */
+
+#ifndef __AW88399_HDA_PROP_H__
+#define __AW88399_HDA_PROP_H__
+
+#include "aw88399_hda.h"
+
+int aw88399_add_properties(struct aw88399_hda *aw88399);
+
+#endif /* __AW88399_HDA_PROP_H__ */
